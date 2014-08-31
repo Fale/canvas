c60c71943961f228a5166cedc92c9ab83efbb115
Fabio Alessandro Locati <fabiolocati@gmail.com>
2014-08-31 19:25:24 +0200

    Add login and logout functions for all pages

--- views.py
+++ views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 from canvas.models import Canvas, Item
 
+def user_logout(request):
+    logout(request)
+    return redirect('/')
+
+@login_required
 def index(request):
     canvas_list = Canvas.objects.all().order_by('name')
     context = {'canvas_list': canvas_list}
     return render(request, 'canvas/index.html', context)
 
+@login_required
 def canvas(request, canvas_id):
     canvas = get_object_or_404(Canvas, id = canvas_id)
     context = {
