6dd344500ef0f93df3d7b3bf8b7ab1f6fa6b50d6
Fabio Alessandro Locati <fabiolocati@gmail.com>
2014-08-31 15:48:49 +0200

    Initial Django project + canvas/models

---
+++ models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Canvas(models.Model):
+    name = models.CharField(max_length = 200)
+
+class Item(models.Model):
+    BOXES = (
+        ('KP', 'Key Partners'),
+        ('KA', 'Key Activities'),
+        ('KR', 'Key Resources'),
+        ('VP', 'Value Proposition'),
+        ('CR', 'Customer Relationships'),
+        ('CH', 'Channels'),
+        ('CS', 'Customer Segments'),
+        ('CSS', 'Cost Structure'),
+        ('RSS', 'Revenue Streams'),
+    )
+
+    BUNDLES = (
+        ('P', 'Product Innovation'),
+        ('C', 'Customer Relationship Management'),
+        ('I', 'Infrastructure Management'),
+    )
+    canvas = models.ForeignKey('Canvas')
+    box = models.CharField(max_lenght = 3, choices = BOXES)
+    bundle = models.CharField(max_lenght = 1, choices = BUNDLES)
+    title = models.CharField(max_length = 200)
