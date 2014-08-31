975b6daffd5a2c6d4a312e1b189e7623c70dd62f
Fabio Alessandro Locati <fabiolocati@gmail.com>
2014-08-31 15:56:26 +0200

    Enable canvas on admin panel

--- admin.py
+++ admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from canvas.models import Canvas, Item
 
-# Register your models here.
+admin.site.register(Canvas)
+admin.site.register(Item)
--- models.py
+++ models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Canvas(models.Model):
     name = models.CharField(max_length = 200)
 
+    class Meta:
+        verbose_name_plural = "Canvasses"
+
 class Item(models.Model):
     BOXES = (
         ('KP', 'Key Partners'),
