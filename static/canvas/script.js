653c7a76dae2fb664cf4433399fe4f8e11262da1
Fabio Alessandro Locati <fabiolocati@gmail.com>
2014-09-02 01:23:36 +0200

    Add edit and delete icons for each item and make delete button delete the item

---
+++ static/canvas/script.js
@@ -0,0 +1,24 @@
+$(".canvas-item").mouseover(function(){
+    var id = $(this).attr('id');
+    $("#" + id + "-edit").show();
+    $("#" + id + "-delete").show();
+});
+
+$(".canvas-item").mouseout(function(){
+    var id = $(this).attr('id');
+    $("#" + id + "-edit").hide();
+    $("#" + id + "-delete").hide();
+});
+
+$(".canvas-item-delete").click(function(){
+    var el = $(this).parent();
+    var id =  $(this).attr('id');
+    var url = $(location).attr('pathname') + id.split('-')[1] + '/delete/';
+    var ajax = $.ajax({
+        type: 'GET',
+        url: url
+    });
+    ajax.complete(function(jqXHR){
+        el.remove();
+    });
+});
--- urls.py
+++ urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^add/$', views.add, name='add'),
     url(r'^(?P<canvas_id>\d+)/$', views.canvas, name='canvas'),
     url(r'^(?P<canvas_id>\d+)/add/(?P<box>\w+)/$', views.addItem, name='addItem'),
+    url(r'^(?P<canvas_id>\d+)/(?P<item_id>\d+)/delete/$', views.deleteItem, name='deleteItem'),
 )
