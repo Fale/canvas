f610af5797bc1bcda2935b25de3ee5fd1ece0437
Fabio Alessandro Locati <fabiolocati@gmail.com>
2014-09-07 15:50:25 +0200

    Move to a more url agnostic linking

--- urls.py
+++ urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, url
-from canvas.views import canvas, item
+from canvas.views import canvas, item, logout
 
 urlpatterns = patterns('',
-    url(r'^$', canvas.list, name='index'),
+    url(r'^$', canvas.list, name='listCanvas'),
     url(r'^add/$', canvas.add, name='addCanvas'),
-    url(r'^(?P<canvas_id>\d+)/$', canvas.show, name='canvas'),
+    url(r'^(?P<canvas_id>\d+)/$', canvas.show, name='showCanvas'),
     url(r'^(?P<canvas_id>\d+)/edit/$', canvas.edit, name='editCanvas'),
     url(r'^(?P<canvas_id>\d+)/delete/$', canvas.delete, name='deleteCanvas'),
     url(r'^(?P<canvas_id>\d+)/add/(?P<box>\w+)/$', item.add, name='addItem'),
